7d2744059066c798cb27403a526a304e13e83560
Sidhartha S <darksoul13570@gmail.com>
2025-01-01 08:17:30 +0530

    Revert "FIX: Fixed Post Creation"
    This reverts commit 0069e6016de494dbe2193ae5f4c70fa9baf03669.

--- client/src/components/CreatePostModal.jsx
+++ client/src/components/CreatePostModal.jsx
@@ -161,15 +161,9 @@ function CreatePostModal({
                   <Spacer />
                 </ModalBody>
                 <ModalFooter>
-                <Button 
-              colorScheme="blue" 
-              mr={3} 
-              onClick={handleSubmit}
-              isLoading={isLoading}
-              loadingText="Posting..."
-            >
-              Post
-            </Button>
+                  <Button colorScheme="blue" mr={3} onClick={handlePost}>
+                    Post
+                  </Button>
                 </ModalFooter>
               </TabPanel>
 
@@ -262,6 +256,15 @@ function CreatePostModal({
           </ModalBody>
           <ModalFooter>
             {error && <Text color="red.500">{error}</Text>}
+            <Button 
+              colorScheme="blue" 
+              mr={3} 
+              onClick={handleSubmit}
+              isLoading={isLoading}
+              loadingText="Posting..."
+            >
+              Post
+            </Button>
           </ModalFooter>        
         </ModalContent>
       </Modal>
